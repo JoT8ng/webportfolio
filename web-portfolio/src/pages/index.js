4e5f9a43075567d1d1ee091aab1f883b645fe58b
Jocelyn Tang <jocelynt8ng@gmail.com>
2023-10-28 23:23:01 +0100

    start adding css

--- web-portfolio/src/pages/index.js
+++ web-portfolio/src/pages/index.js
@@ -8,29 +8,36 @@ const Home = () => {
     return (
         <Layout>
             <div>
-                <h1>Hi,</h1>
-                <h1>I'm Jocelyn Tang, a developer in London</h1>
-                <p>Description</p>
-                <Link href='/about'>About me</Link>
-                <div className='lowbar-icons'>
-                    <Link href='https://github.com/JoT8ng/'>
-                        <BsGithub style={{width: '30px', height: '30px', color: 'rgb(208,241,241)'}} />
-                    </Link>
-                    <Link href='https://uk.linkedin.com/in/jocelyntang'>
-                        <BsLinkedin style={{width: '50px', height: '50px', color: 'rgb(208,241,241)', paddingLeft: '25px'}} />
-                    </Link>
-                </div>
-            </div>
-            <div>
-                {data.Projects.map(project => (
-                    <div key={project.name}>
-                        <Link href={`/projects/${project.internal}`}>
-                            <p>{project.name}</p>
+                <section className='flex flex-col px-52 py-44 circular-gradient-light'>
+                    <h1 className='font-sans font-normal text-4xl pb-3'>Hi,</h1>
+                    <h1 className='font-sans font-normal text-4xl pb-3'>I'm Jocelyn Tang, a web developer in London</h1>
+                    <p className='font-mono text-base py-3'>
+                        I create beautiful and robust web applications with a profound understanding of the importance of 
+                        client and user requirements at the core of every successful project.
+                    </p>
+                    <Link href='/about' className='font-roboto-bold font-bold text-lg py-3'>About me</Link>
+                    <div className='flex gap-4'>
+                        <Link href='https://github.com/JoT8ng/'>
+                            <BsGithub className='w-5 h-5 text-black' />
                         </Link>
-                        <Link href={project.link}>Link</Link>
-                        <Link href={project.github}>Github</Link>
+                        <Link href='https://uk.linkedin.com/in/jocelyntang'>
+                            <BsLinkedin className='w-5 h-5 text-black' />
+                        </Link>
                     </div>
-                ))}
+                </section>
+                <section className='grid p-10 bg-gray-light xs:grid-cols-1 lg:grid-cols-2 gap-8 content-center justify-items-center'>
+                    {data.Projects.map(project => (
+                        <div key={project.name} className='h-full w-full xs:h-auto xs:rectangle overflow-hidden shadow-md bg-gray-100 px-20 py-40'>
+                            <div className='justify-between text-center'>
+                                <Link href={`/projects/${project.internal}`}>
+                                    <p>{project.name}</p>
+                                </Link>
+                                <Link href={project.link}>Link</Link>
+                                <Link href={project.github}>Github</Link>
+                            </div>
+                        </div>
+                    ))}
+                </section>
             </div>
         </Layout>
     )
--- web-portfolio/tailwind.config.js
+++ web-portfolio/tailwind.config.js
@@ -6,11 +6,51 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'white': '#FFFFFF',
+      'black': '#000000',
+      'orange': '#FF9866',
+      'orange-light': '#FF9966',
+      'salmon': {
+        100: '#EF5456',
+        200: '#FF6063',
+        300: '#F05355',
+        400: '#F05255',
+        500: '#3E2215',
+        600: '#F05254',
+        700: '#C94E51',
+      },
+      'salmon-dark': '#F05355',
+      'green-dark': '#1D2603',
+      'green': '#E1ECC2',
+      'green-light': '#E1EBC1',
+      'gray-dark': '#151515',
+      'gray-light': '#F2F2F2',
+      'gray': {
+        100: '#ADADAD',
+        200: '#6F6F6F',
+        300: '#595959',
+      },
+    },
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui', 'Roboto'],
+      'mono': ['ui-monospace', 'Consolas'],
+      'roboto-bold': ['Roboto', 'sans-serif', 'black'],
+    },
+    fontWeight: {
+      normal: '500',
+      bold: '700',
+      black: '900',
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      backgroundColor: {
+        'default': 'rgb(225,236,194)',
       },
     },
   },
