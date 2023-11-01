6bd3bf05f93051b4f47b34e38b40b433859ecf6e
Jocelyn Tang <jocelynt8ng@gmail.com>
2023-11-01 23:21:04 +0000

    fix navbar css

--- web-portfolio/src/components/navbar.js
+++ web-portfolio/src/components/navbar.js
@@ -12,28 +12,28 @@ const Navbar = () => {
     }
 
     const content = (
-        <div className='md:hidden bg-gray-light left-0 top-[-6%] w-full flex items-center justify-end px-5'>
+        <div className='md:hidden absolute bg-gray-light left-0 right-0 top-16 w-full flex items-center justify-start px-8 pb-8'>
             <ul className='flex flex-col gap-8'>
                 <li>
-                    <Link href='/' activeClassName='active'>Projects</Link>
+                    <Link href='/' activeClassName='active' className='font-sans font-normal text-sm text-black hover:text-gradient'>Projects</Link>
                 </li>
                 <li>
-                    <Link href='/gisprojects' activeClassName='active'>GIS Projects</Link>
+                    <Link href='/gisprojects' activeClassName='active' className='font-sans font-normal text-sm text-black hover:text-gradient'>GIS Projects</Link>
                 </li>
                 <li>
-                    <Link href='/about' activeClassName='active'>About</Link>
+                    <Link href='/about' activeClassName='active' className='font-sans font-normal text-sm text-black hover:text-gradient'>About</Link>
                 </li>
                 <li>
-                    <Link href='/resume' activeClassName='active'>Resume</Link>
+                    <Link href='/resume' activeClassName='active' className='font-sans font-normal text-sm text-black hover:text-gradient'>Resume</Link>
                 </li>
                 <li>
                     <Link href='https://github.com/JoT8ng/'>
-                        <BsGithub style={{width: '30px', height: '30px', color: 'rgb(208,241,241)'}} />
+                        <BsGithub className='w-5 h-5 text-black hover:text-salmon-100' />
                     </Link>
                 </li>
                 <li>
                     <Link href='https://uk.linkedin.com/in/jocelyntang'>
-                        <BsLinkedin style={{width: '50px', height: '50px', color: 'rgb(208,241,241)', paddingLeft: '25px'}} />
+                        <BsLinkedin className='w-5 h-5 text-black hover:text-salmon-100' />
                     </Link>
                 </li>
             </ul>
@@ -41,46 +41,48 @@ const Navbar = () => {
     )
 
     return(
-        <nav className='flex justify-between mx-auto bg-green-dark pl-10 pr-10 py-5'>
-            <div className='bg-orange'>
-                <Link href='/'>
-                    <span className='items-start font-roboto-bold font-bold text-black'>JT.</span>
-                </Link>
+        <nav className='bg-gray-light'>
+            <div className='flex justify-between flex-1 p-5'>
+                <div>
+                    <Link href='/'>
+                        <span className='items-start font-roboto-bold font-bold text-black'>JT.</span>
+                    </Link>
+                </div>
+                <div className='hidden md:flex md:static md:flex-1 text-center sm:hidden justify-end'>
+                    <ul className='flex md:gap-8'>
+                        <li>
+                            <Link href='/' activeClassName='active' className='font-sans font-normal text-sm text-black hover:text-gradient'>Projects</Link>
+                        </li>
+                        <li>
+                            <Link href='/gisprojects' activeClassName='active' className='font-sans font-normal text-sm text-black hover:text-gradient'>GIS Projects</Link>
+                        </li>
+                        <li>
+                            <Link href='/about' activeClassName='active' className='font-sans font-normal text-sm text-black hover:text-gradient'>About</Link>
+                        </li>
+                        <li>
+                            <Link href='/resume' activeClassName='active:text-gradient' className='font-sans font-normal text-sm text-black hover:text-gradient active:text-gradient'>Resume</Link>
+                        </li>
+                        <li>
+                            <Link href='https://github.com/JoT8ng/'>
+                                <BsGithub className='w-5 h-5 text-black hover:text-salmon-100' />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href='https://uk.linkedin.com/in/jocelyntang'>
+                                <BsLinkedin className='w-5 h-5 text-black hover:text-salmon-100' />
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    {menuClick && content}
+                </div>
+                <div className='flex items-center gap-6'>
+                    <button className='block md:hidden transition' onClick={handleClick}>
+                        {menuClick ? <IoMdClose className='w-6 h-6' /> : <IoIosMenu className='w-6 h-6' />}
+                    </button>
+                </div>
             </div>
-            <div className='sm:hidden md:static bg-salmon-100 md:min-h-fit md:w-auto md:flex items-center justify-items-center px-5'>
-                <ul className='flex md:flex-row  md:items-center md:gap-[4vw]'>
-                    <li>
-                        <Link href='/' activeClassName='active'>Projects</Link>
-                    </li>
-                    <li>
-                        <Link href='/gisprojects' activeClassName='active'>GIS Projects</Link>
-                    </li>
-                    <li>
-                    	<Link href='/about' activeClassName='active'>About</Link>
-                    </li>
-                    <li>
-                    	<Link href='/resume' activeClassName='active'>Resume</Link>
-                    </li>
-                    <li>
-                    	<Link href='https://github.com/JoT8ng/'>
-                        	<BsGithub style={{width: '30px', height: '30px', color: 'rgb(208,241,241)'}} />
-                    	</Link>
-                    </li>
-                    <li>
-                    	<Link href='https://uk.linkedin.com/in/jocelyntang'>
-                        	<BsLinkedin style={{width: '50px', height: '50px', color: 'rgb(208,241,241)', paddingLeft: '25px'}} />
-                    	</Link>
-                    </li>
-            	</ul>
-            </div>
-            <div>
-		        {menuClick && content}
-	        </div>
-	        <div className='flex items-center gap-6'>
-		        <button className='block md:hidden transition' onClick={handleClick}>
-		            {menuClick ? <IoMdClose /> : <IoIosMenu />}
-		        </button>
-	        </div>
         </nav>
     );
 }
