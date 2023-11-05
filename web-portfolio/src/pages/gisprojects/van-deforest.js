0e14ba7779df6d46a724a2dcf54d946699d0c51b
Jocelyn Tang <125464101+JoT8ng@users.noreply.github.com>
2023-11-05 12:41:55 +0000

    Update van-deforest.js

--- web-portfolio/src/pages/gisprojects/van-deforest.js
+++ web-portfolio/src/pages/gisprojects/van-deforest.js
@@ -1,6 +1,8 @@
-import Template from '../../components/template'
+import Template from '../../components/template2'
 import Layout from '../../components/layout'
 import data from '../../data/data'
+import Image from 'next/image'
+import classification from '../../media/van-deforest/MtHorneOldGrowthDeforestationClassified-01.jpg'
 
 
 const Vandeforest = () => {
@@ -8,6 +10,30 @@ const Vandeforest = () => {
         <Layout>
             <div>
                 <Template info={data.GISProjects[3]} />
+                <div className='bg-gray-light pb-20 px-20 flex justify-items-center gap-8'>
+                    <div className='group flex-col overflow-hidden shadow-md bg-green-light flex justify-start p-8'>
+                        <p className='font-sans font-bold text-sm text-black group-hover:text-salmon-100 pb-3'>Software Used:</p>
+                        <ul>
+                            <li className='font-mono text-xs text-black group-hover:text-salmon-100'>QGIS</li>
+                            <li className='font-mono text-xs text-black group-hover:text-salmon-100'>Adobe Illustrator</li>
+                            <li className='font-mono text-xs text-black group-hover:text-salmon-100'>Adobe Photoshop</li>
+                            <li className='font-mono text-xs text-black group-hover:text-salmon-100'>MultiSpec- Multispectral Image Data Analysis System</li>
+                            <li className='font-mono text-xs text-black group-hover:text-salmon-100'>Landsat 8, 7, 5 Satellite</li>
+                            <li className='font-mono text-xs text-black group-hover:text-salmon-100'>Copernicus Sentinel</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className='flex flex-col bg-gray-light grid-cols-1 gap-8 md:px-20 sm:px-10 pb-20'>
+                        <Image src={classification} />
+                        <video controls>
+                            <source src='../../media/van-deforest/OldGrowthDeforestation_MtHorneArea.avi' />
+                            Your browser does not support the video tag.
+                        </video>
+                        <video controls>
+                            <source src='../../media/van-deforest/OldGrowthDeforestation_MtHorneAreaIR.avi' />
+                            Your browser does not support the video tag.
+                        </video>
+                </div>
             </div>
         </Layout>
     )
