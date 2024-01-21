c443e8d971308db92c945cd5e994731102c558e3
Jocelyn Tang <jocelynt8ng@gmail.com>
2024-01-21 15:18:21 +0000

    add full stack open projects

--- web-portfolio/src/pages/index.js
+++ web-portfolio/src/pages/index.js
@@ -1,18 +1,18 @@
 import Link from 'next/link';
-import { useState } from 'react';
 import Layout from '../components/layout'
 import { BsGithub, BsLinkedin } from "react-icons/bs"
 import data from '../data/data'
 import Image from 'next/image'
 import proimg1 from '../media/hydrodamsapp.jpg'
-import proimg2 from '../media/README_HomePic.png'
+import proimg2 from '../media/epdata-thumbnail.png'
+import proimg3 from '../media/Placeholder.png'
 import group1 from '../media/GroupLight.png'
 import GroupDark from '../media/GroupDark.png'
 import { useTheme } from '../context/ThemeProvider'
 
 
 const Home = () => {
-    const projectImage = { proimg1, proimg2, group1 }
+    const projectImage = { proimg1, proimg2, group1, proimg3 }
     const { isDarkMode, toggleTheme } = useTheme()
 
     return (
@@ -33,6 +33,9 @@ const Home = () => {
                             <Link href='https://github.com/JoT8ng/'>
                                 <BsGithub className='w-5 h-5 text-black dark:text-white' />
                             </Link>
+                            <Link href='https://www.linkedin.com/in/jocelyntang/'>
+                                <BsLinkedin className='w-5 h-5 text-black dark:text-white hover:text-salmon-100' />
+                            </Link>
                         </div>
                     </div>
                     <div className='flex-row hover:opacity-80'>
@@ -51,11 +54,11 @@ const Home = () => {
                     {data.Projects.map(project => (
                         <div key={`${project.name}1`} className='relative overflow-hidden'>
                             <div key={`${project.name}2`} className='relative aspect-w-16 aspect-h-9'>
-                                <Image src={projectImage[project.thumbnail]} alt={project.name} className='h-auto max-w-full shadow-md opacity-100 hover:opacity-30 transition duration-300' priority />
+                                <Image src={projectImage[project.thumbnail]} alt={project.name} className='rectangle shadow-md opacity-100 hover:opacity-30 transition duration-300' priority />
                             </div>
                             <div key={`${project.name}3`} className='absolute top-0 left-0 w-full rectangle overflow-hidden shadow-md opacity-0 hover:opacity-70 transition duration-300 bg-gray-dark  flex justify-center items-center px-6'>
                                 <div key={`${project.name}4`} className='text-center'>
-                                    <Link href={`/projects/${project.internal}`}>
+                                    <Link href={project.internal}>
                                         <p className='font-roboto-bold font-bold pb-3 text-gray-light'>{project.name}</p>
                                     </Link>
                                     <Link href={project.link} className='p-3 text-sm text-gradient bg-clip-text text-transparent'>Link</Link>
--- web-portfolio/src/pages/projects.js
+++ web-portfolio/src/pages/projects.js
@@ -5,23 +5,24 @@ import Image from 'next/image'
 import proimg1 from '../media/hydrodamsapp.jpg'
 import proimg2 from '../media/README_HomePic.png'
 import group1 from '../media/GroupLight.png'
+import proimg3 from '../media/Placeholder.png'
 
 
 const GisProjects = () => {
-    const projectImage = { proimg1, proimg2, group1 }
+    const projectImage = { proimg1, proimg2, group1, proimg3 }
 
     return (
         <Layout>
             <div>
-            <section className='min-h-screen grid bg-gray-light dark:bg-gray-dark p-10 xs:grid-cols-1 lg:grid-cols-2 gap-8 content-start justify-center md:h-screen'>
+            <section className='grid bg-gray-light dark:bg-gray-dark p-10 xs:grid-cols-1 lg:grid-cols-2 gap-8 content-center justify-items-center'>
                     {data.Projects.map(project => (
                         <div key={`${project.name}1`} className='relative overflow-hidden'>
                             <div key={`${project.name}2`} className='relative aspect-w-16 aspect-h-9'>
-                                <Image src={projectImage[project.thumbnail]} alt={project.name} className='h-auto max-w-full shadow-md opacity-100 hover:opacity-30 transition duration-300' priority />
+                                <Image src={projectImage[project.thumbnail]} alt={project.name} className='rectangle shadow-md opacity-100 hover:opacity-30 transition duration-300' priority />
                             </div>
                             <div key={`${project.name}3`} className='absolute top-0 left-0 w-full rectangle overflow-hidden shadow-md opacity-0 hover:opacity-70 transition duration-300 bg-gray-dark  flex justify-center items-center px-6'>
                                 <div key={`${project.name}4`} className='text-center'>
-                                    <Link href={`/projects/${project.internal}`}>
+                                    <Link href={project.internal}>
                                         <p className='font-roboto-bold font-bold pb-3 text-gray-light'>{project.name}</p>
                                     </Link>
                                     <Link href={project.link} className='p-3 text-sm text-gradient bg-clip-text text-transparent'>Link</Link>
